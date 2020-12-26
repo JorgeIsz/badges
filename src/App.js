1d55fcab2a82df62c70db810ea7398097254f87a
Jorge Isaza <isaza.jorge@hotmail.com>
2020-12-26 14:09:29 -0500

    Agregado: Home y NotFound

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 import Badges from './pages/Badges';
 import BadgeNew from './pages/BadgeNew'
 import Layout from './components/Layout'
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
 
 function App() {
   return (
 	<BrowserRouter>
 	  <Layout>
 	  	<Switch>
-	  	  <Route exact path="/badges/new" component={BadgeNew}/>
+	  	  <Route exact path="/" component={Home}/>
 	  	  <Route exact path="/badges" component={Badges}/>
+	  	  <Route exact path="/badges/new" component={BadgeNew}/>
+	  	  <Route component={NotFound }/>
 	  	</Switch>
 	  </Layout>
 	</BrowserRouter>
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './styles/Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+    	<div className="home">
+			HomePage
+    	</div>
+    )
+  }
+}
